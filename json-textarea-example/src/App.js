9e8717f2639a24badc0ee0bcac1a805d85d389b3
Moslem Azad .Sh <tweak.sara@gmail.com>
2021-07-31 22:10:05 +0430

    a little tweek on styles !

--- json-textarea-example/src/App.js
+++ json-textarea-example/src/App.js
@@ -1,20 +1,25 @@
 import JTexarea from "./json-textarea/JTexarea";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [valid, setValid] = useState(true);
   //called when the input is successfully parsed to js object
   function onPass(object) {
+    setValid(true);
     console.log("PASSED: ", object);
   }
 
   //called while there is an error
   function onError(value, error) {
+    setValid(false);
     console.log(value, error);
   }
 
   return (
     <div className="container">
       <JTexarea onPass={onPass} onError={onError} />
+      <h3>{valid ? "Valid Json :)" : "Invalid JSON :("}</h3>
     </div>
   );
 }
